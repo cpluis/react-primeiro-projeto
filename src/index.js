a901d932cbc2aad471b8a6275ff6bc4a4665478c
cpluis <luis.pires@softdesign.com.br>
2021-07-26 23:20:35 -0300

    atualizando projeto

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
 
-
+ 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
-);
+); 
