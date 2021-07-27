593abe7e469a4070afb319b1d62c44f56865da02
cpluis <luis.pires@softdesign.com.br>
2021-07-26 23:05:24 -0300

    excluindo os imports que não uso

--- src/components/Button/style.js
+++ src/components/Button/style.js
@@ -1,6 +1,5 @@
 import styled from "styled-components";
 
-
 export const btn_filmes = styled.button`
 width: 800px;
  
--- src/pages/Home/style.js
+++ src/pages/Home/style.js
@@ -4,7 +4,7 @@ import styled from "styled-components";
 export const Filme = styled.div`
 
 
-display:flex
+display:flex;
 flex-wrap:wrap;
 
 `;
